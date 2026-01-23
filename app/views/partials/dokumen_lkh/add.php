b1f8b113952d81305814e518bbffbfb9294f0867
Blogotes <96991667+Blogotes@users.noreply.github.com>
2026-01-23 16:10:55 +0700

    Add files via upload

---
+++ app/views/partials/dokumen_lkh/add.php
@@ -0,0 +1,100 @@
+<?php
+$comp_model = new SharedController;
+$page_element_id = "add-page-" . random_str();
+$current_page = $this->set_current_page_link();
+$csrf_token = Csrf::$token;
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+?>
+<section class="page" id="<?php echo $page_element_id; ?>" data-page-type="add"  data-display-type="" data-page-url="<?php print_link($current_page); ?>">
+    <?php
+    if( $show_header == true ){
+    ?>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col ">
+                    <h4 class="record-title">Add New Dokumen Lkh</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <div  class="">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 comp-grid">
+                    <?php $this :: display_page_errors(); ?>
+                    <div  class="bg-light p-3 animated fadeIn page-content">
+                        <form id="dokumen_lkh-add-form" role="form" novalidate enctype="multipart/form-data" class="form page-form form-horizontal needs-validation" action="<?php print_link("dokumen_lkh/add?csrf_token=$csrf_token") ?>" method="post">
+                            <div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="Nama_Penyuluh">Nama Penyuluh <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <input id="ctrl-Nama_Penyuluh"  value="<?php  echo $this->set_field_value('Nama_Penyuluh',""); ?>" type="text" placeholder="Enter Nama Penyuluh"  required="" name="Nama_Penyuluh"  class="form-control " />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="KUA_Kecamatan">Kua Kecamatan <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <input id="ctrl-KUA_Kecamatan"  value="<?php  echo $this->set_field_value('KUA_Kecamatan',""); ?>" type="text" placeholder="Enter Kua Kecamatan"  required="" name="KUA_Kecamatan"  class="form-control " />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group ">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="control-label" for="LKH_Bulan">Lkh Bulan <span class="text-danger">*</span></label>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <div class="">
+                                                        <input id="ctrl-LKH_Bulan"  value="<?php  echo $this->set_field_value('LKH_Bulan',""); ?>" type="text" placeholder="Enter Lkh Bulan"  required="" name="LKH_Bulan"  class="form-control " />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group ">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <label class="control-label" for="file">File <span class="text-danger">*</span></label>
+                                                    </div>
+                                                    <div class="col-sm-8">
+                                                        <div class="">
+                                                            <div class="dropzone required" input="#ctrl-file" fieldname="file"    data-multiple="false" dropmsg="Choose files or drag and drop files to upload"    btntext="Browse" filesize="3" maximum="1">
+                                                                <input name="file" id="ctrl-file" required="" class="dropzone-input form-control" value="<?php  echo $this->set_field_value('file',""); ?>" type="text"  />
+                                                                    <!--<div class="invalid-feedback animated bounceIn text-center">Please a choose file</div>-->
+                                                                    <div class="dz-file-limit animated bounceIn text-center text-danger"></div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group form-submit-btn-holder text-center mt-3">
+                                                <div class="form-ajax-status"></div>
+                                                <button class="btn btn-primary" type="submit">
+                                                    Simpan
+                                                    <i class="fa fa-send"></i>
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
