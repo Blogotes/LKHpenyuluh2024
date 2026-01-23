43d698d3337f3ab305a17efa51fcb6cd6da443b6
Blogotes <96991667+Blogotes@users.noreply.github.com>
2026-01-23 16:13:30 +0700

    Add files via upload

---
+++ helpers/Menu.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbartopleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'dokumen_lkh', 
+			'label' => 'Dokumen Lkh', 
+			'icon' => ''
+		)
+	);
+		
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => ''
+		),
+		
+		array(
+			'path' => 'dokumen_lkh', 
+			'label' => 'Dokumen Lkh', 
+			'icon' => ''
+		)
+	);
+		
+	
+	
+}
